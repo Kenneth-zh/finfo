4430aec4d509450b58c64548bce16a4099559877
kenneth <zhangqizhi929@gmail.com>
2025-11-03 20:41:23 +0800

    ready for pyo3

--- examples/query.rs
+++ examples/query.rs
@@ -3,6 +3,9 @@ use dotenv::dotenv;
 use finfo::query::FlightSqlClient;
 use std::env;
 
+use df_interchange::Interchange;
+use polars::prelude::*;
+
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().ok();
@@ -14,7 +17,9 @@ async fn main() -> Result<()> {
     let query_sql = "SELECT * FROM kline WHERE symbol = 'AAPL.US'";
 
     // 执行查询
-    let df = client.execute_sql(query_sql).await?;
+    let batches = client.execute_sql(query_sql).await?;
+
+    let df = Interchange::from_arrow_56(batches)?.to_polars_0_51()?;
 
     println!("{:?}", df);
 
